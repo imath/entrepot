96db87d7f13f3091cb01fe8dfd153177f397d132
imath <math.viet@gmail.com>
2017-05-27 17:26:58 +0200

    Prevent the search tab to appear on the Plugin Install screen when viewing the Galerie tab

--- js/galerie.js
+++ js/galerie.js
@@ -5,6 +5,11 @@
 		return false;
 	}
 
+	// Prevent the search tab to appear
+	$( document ).ready( function() {
+		$( 'body' ).removeClass( 'plugin-install-php' ).addClass( 'gallerie-install-php' );
+	} );
+
 	$.getJSON( Galerie.url, function( data ) {
 		// Shuffle results
 		data.sort( function() { return 0.5 - Math.random() } );
