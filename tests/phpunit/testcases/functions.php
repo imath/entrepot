7801f196b49f684e5400032c2fa3014bc7781efb
imath <math.viet@gmail.com>
2017-05-31 16:39:50 +0200

    More unit tests for version checks

--- tests/phpunit/testcases/functions.php
+++ tests/phpunit/testcases/functions.php
@@ -12,11 +12,92 @@ class galerie_Functions_Tests extends WP_UnitTestCase {
 		return PR_TESTING_ASSETS;
 	}
 
-	public function test_galerie_get_plugin_latest_stable_release() {
+	/**
+	 * @group update
+	 */
+	public function test_galerie_get_plugin_latest_stable_release_for_update() {
 		$stable = PR_TESTING_ASSETS . '/releases-stable.atom';
-		$release = galerie_get_plugin_latest_stable_release( $stable );
 
-		$this->assertTrue( is_object( $release ) );
+		add_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$json = galerie_get_repository_json( 'test-plugin' );
+
+		$release = galerie_get_plugin_latest_stable_release( $stable, array(
+			'plugin'            => $json->name,
+			'slug'              => 'test-plugin',
+			'Version'           => '1.0.0-beta1',
+			'GitHub Plugin URI' => rtrim( $json->releases, '/releases' ),
+		) );
+
+		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$this->assertTrue( $release->is_update );
+	}
+
+	/**
+	 * @group update
+	 */
+	public function test_galerie_get_plugin_not_stable_release_for_update() {
+		$stable = PR_TESTING_ASSETS . '/releases-not-stable.atom';
+
+		add_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$json = galerie_get_repository_json( 'test-plugin' );
+
+		$release = galerie_get_plugin_latest_stable_release( $stable, array(
+			'plugin'            => $json->name,
+			'slug'              => 'test-plugin',
+			'Version'           => '1.0.0-beta1',
+			'GitHub Plugin URI' => rtrim( $json->releases, '/releases' ),
+		) );
+
+		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$this->assertFalse( $release->is_update );
+	}
+
+	/**
+	 * @group update
+	 */
+	public function test_galerie_get_plugin_beta_latest_release_for_update() {
+		$stable = PR_TESTING_ASSETS . '/releases-beta-after-stable.atom';
+
+		add_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$json = galerie_get_repository_json( 'test-plugin' );
+
+		$release = galerie_get_plugin_latest_stable_release( $stable, array(
+			'plugin'            => $json->name,
+			'slug'              => 'test-plugin',
+			'Version'           => '1.7.0',
+			'GitHub Plugin URI' => rtrim( $json->releases, '/releases' ),
+		) );
+
+		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$this->assertFalse( $release->is_update );
+	}
+
+	/**
+	 * @group update
+	 */
+	public function test_galerie_get_plugin_beta_latest_release_but_update() {
+		$stable = PR_TESTING_ASSETS . '/releases-beta-after-stable.atom';
+
+		add_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$json = galerie_get_repository_json( 'test-plugin' );
+
+		$release = galerie_get_plugin_latest_stable_release( $stable, array(
+			'plugin'            => $json->name,
+			'slug'              => 'test-plugin',
+			'Version'           => '1.6.0',
+			'GitHub Plugin URI' => rtrim( $json->releases, '/releases' ),
+		) );
+
+		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$this->assertTrue( $release->is_update );
 	}
 
 	/**
@@ -37,7 +118,53 @@ class galerie_Functions_Tests extends WP_UnitTestCase {
 		) );
 
 		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
-		
+
 		$this->assertTrue( $release->is_install );
+	}
+
+	/**
+	 * @group install
+	 */
+	public function test_galerie_get_plugin_not_stable_release_for_install() {
+		$stable = PR_TESTING_ASSETS . '/releases-not-stable.atom';
+
+		add_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$json = galerie_get_repository_json( 'test-plugin' );
+
+		$release = galerie_get_plugin_latest_stable_release( $stable, array(
+			'plugin'            => $json->name,
+			'slug'              => 'test-plugin',
+			'Version'           => 'latest',
+			'GitHub Plugin URI' => rtrim( $json->releases, '/releases' ),
+		) );
+
+		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$this->assertTrue( ! isset( $release->is_install ) );
+		$this->assertFalse( $release->is_update );
+	}
+
+	/**
+	 * @group install
+	 */
+	public function test_galerie_get_plugin_beta_after_release_for_install() {
+		$stable = PR_TESTING_ASSETS . '/releases-beta-after-stable.atom';
+
+		add_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$json = galerie_get_repository_json( 'test-plugin' );
+
+		$release = galerie_get_plugin_latest_stable_release( $stable, array(
+			'plugin'            => $json->name,
+			'slug'              => 'test-plugin',
+			'Version'           => 'latest',
+			'GitHub Plugin URI' => rtrim( $json->releases, '/releases' ),
+		) );
+
+		remove_filter( 'galerie_plugins_dir', array( $this, 'repositories_dir' ) );
+
+		$this->assertTrue( $release->is_install );
+		$this->assertTrue( $release->version === '1.7.0' );
 	}
 }
