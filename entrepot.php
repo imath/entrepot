6d42ee3f5da9db01f3e74de691aeabbf0e22168a
imath <math.viet@gmail.com>
2018-05-31 09:54:03 +0200

    Bump plugin version to 1.4.0-alpha

--- entrepot.php
+++ entrepot.php
@@ -3,7 +3,7 @@
  * Plugin Name: Entrepôt
  * Plugin URI: https://github.com/imath/entrepot/
  * Description: Une liste d'extensions gratuites hébergées sur GitHub.com.
- * Version: 1.3.0
+ * Version: 1.4.0-alpha
  * Requires at least: 4.8
  * Tested up to: 5.0
  * License: GNU/GPL 2
@@ -69,7 +69,7 @@ final class Entrepot {
 	 */
 	private function globals() {
 		// Version
-		$this->version = '1.3.0';
+		$this->version = '1.4.0-alpha';
 
 		// Domain
 		$this->domain = 'entrepot';
