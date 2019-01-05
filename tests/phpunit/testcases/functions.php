d33ede4105aa534917928332400fcf63fdc0cdfa
imath <math.viet@gmail.com>
2019-01-05 14:12:15 +0100

    Fix failing tests requiring WordPress 5.0

--- tests/phpunit/testcases/functions.php
+++ tests/phpunit/testcases/functions.php
@@ -541,48 +541,4 @@ class entrepot_Functions_Tests extends WP_UnitTestCase {
 		$test_upgrade_db_version = $reset_global;
 		entrepot()->upgrades = $reset;
 	}
-
-	/**
-	 * @group blocks
-	 */
-	public function test_entrepot_blocks_dir() {
-		add_filter( 'entrepot_blocks_dir', array( $this, 'blocks_dir' ) );
-
-		wp_cache_delete( 'blocks', 'entrepot' );
-		$blocks = entrepot_get_blocks();
-		$this->assertTrue( 1 === count( $blocks ) );
-
-		$block = reset( $blocks );
-		$this->assertTrue( 'random-block' === wp_basename( $block->path ) );
-
-		remove_filter( 'entrepot_blocks_dir', array( $this, 'blocks_dir' ) );
-	}
-
-	/**
-	 * @group blocks
-	 */
-	public function test_entrepot_blocks_dir_random_block() {
-		add_filter( 'entrepot_blocks_dir', array( $this, 'blocks_dir' ) );
-
-		wp_cache_delete( 'blocks', 'entrepot' );
-		$block = entrepot_get_blocks( 'random-block' );
-
-		$this->assertTrue( 'random-block' === wp_basename( $block->path ) );
-
-		remove_filter( 'entrepot_blocks_dir', array( $this, 'blocks_dir' ) );
-	}
-
-	/**
-	 * @group blocks
-	 */
-	public function test_entrepot_blocks_dir_random_folder() {
-		add_filter( 'entrepot_blocks_dir', array( $this, 'blocks_dir' ) );
-
-		wp_cache_delete( 'blocks', 'entrepot' );
-		$block = entrepot_get_blocks( 'random-folder' );
-
-		$this->assertEmpty( $block );
-
-		remove_filter( 'entrepot_blocks_dir', array( $this, 'blocks_dir' ) );
-	}
 }
